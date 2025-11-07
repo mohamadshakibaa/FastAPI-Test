36851b7ef84e1779f4ef1850a9fb59e5e4cb8a94
mohamadshakibaa <www.mohamadrasol123@gmail.com>
2025-11-07 19:46:48 +0330

    set UUID

--- main2.py
+++ main2.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI, Query, Path, Body
 from typing import Optional
 from pydantic import BaseModel, Field
+from uuid import UUID
+from datetime import datetime, time, timedelta
 
 
 app = FastAPI()
@@ -39,3 +41,31 @@ class Item2(BaseModel):
 async def update_item(item_id: int, item: Item2):
     result = {"item_id": item_id, "item": item}
     return result
+
+
+
+
+
+# UUID
+# you must set uuid in the terminal => 
+#  import uuid 
+#  from uuid import uuid4
+#  uuid4()
+@app.put("/items_uuid/{item_id}")
+async def update_item(
+    item_id: UUID,
+    start_date: Optional[datetime] = Body(None),
+    end_time: Optional[datetime] = Body(None),
+    repeat_at: Optional[time] = Body(None),
+    process_after: Optional[timedelta] = Body(None)
+):
+    start_process = start_date + process_after
+    duration = end_time - start_process
+    return {"item_id": item_id,
+            "start_date": start_date,
+            "end_time": end_time,
+            "repeat_at": repeat_at,
+            "process_after": process_after,
+            "start_process": start_process,
+            "duration": duration
+}
