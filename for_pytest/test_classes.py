68b1501343f9cedc9cdaaeb5e974fd76dca03fe4
mohamadshakibaa <www.mohamadrasol123@gmail.com>
2026-02-15 17:33:32 +0330

    pytest in class

--- for_pytest/test_classes.py
+++ for_pytest/test_classes.py
@@ -1,7 +1,23 @@
 from classes import AnonymousSurvey
+import pytest
 
-def test_store_single_response():
+
+# def test_store_single_response():
+#     question = "What Language did you first learn to speak?"
+#     language_survey = AnonymousSurvey(question)
+#     language_survey.store_responses("English")
+#     assert "English" in language_survey.responses
+    
+    
+    
+    
+@pytest.fixture
+def language_survey():
     question = "What Language did you first learn to speak?"
     language_survey = AnonymousSurvey(question)
+    return language_survey
+
+def test_store_single_response(language_survey):
     language_survey.store_responses("English")
     assert "English" in language_survey.responses
+    
